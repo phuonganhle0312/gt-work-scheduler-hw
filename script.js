c8b7275adc52523b707b700ececfd3a3c9e72bd3
Phuong Le <phuonganh.le0312@gmail.com>
2020-06-15 17:33:08 -0400

    added html and css and js file with code provided by curriculum team. got current time from moment and declared variables.

---
+++ script.js
@@ -0,0 +1,12 @@
+$(document).ready(function() {
+// current time from moment
+const currentTime = moment().format('MMMM Do YYYY, h:mm:ss a');
+//made date heading the current time
+let $dateHeading = $("#currentDay");
+$dateHeading.text(currentTime);
+// testing variable
+const test= false;
+
+
+
+});
