9819ca5f8fb852dddc5cf528afa24b0e1fe6c0ef
Phuong Le <phuonganh.le0312@gmail.com>
2020-06-15 18:37:26 -0400

    started building plan blocks

--- script.js
+++ script.js
@@ -1,27 +1,42 @@
-$(document).ready(function() {
-// current time from moment
-const currentTime = moment().format('MMMM Do YYYY, h:mm:ss a');
-//made date heading the current time
-let $dateHeading = $('#currentDay');
-$dateHeading.text(currentTime);
-// testing variable
-const test= false;
-//test in non standard hours
-let nowHour24 = moment().format('H');
-let nowHour12 = moment().format('h');
-//setting time for non standard hours
-if (test){
-    nowHour24= 13;
-    nowHour12= 1;
-}
-//getting todos from localStorage. JSON parse string into object
-let savedPlans = JSON.parse(localStorage.getItem("savedPlans"));
-if (test) {console.log(savedPlans);}
-//local storage plans update plan array
-if (savedPlans !== null){
-    planArray = savedPlans;
-}else {
-    planArray=new Array(9);
-}
+$(document).ready(function () { // current time from moment
+    const currentTime = moment().format('MMMM Do YYYY, h:mm:ss a');
+    // made date heading the current time
+    let $dateHeading = $('#currentDay');
+    $dateHeading.text(currentTime);
+    // testing variable
+    const test = false;
+    // test in non standard hours
+    let nowHour24 = moment().format('H');
+    let nowHour12 = moment().format('h');
+    // setting time for non standard hours
+    if (test) {
+        nowHour24 = 13;
+        nowHour12 = 1;
+    }
+    // plan block variable
+    let $planBlocks = $('#timeblocks');
+    $planBlocks.empty();
+    // fixed 9-5 work day hours with an index for array for offset from hour
+    for (let hour = 9; hour <= 17; hour++) {
+        let index = hour - 9;
+
+    //building plan blocks
+    let $planRow= $('<div>');
+    $planRow.addClass('row');
+    $planRow.attr('hourIndex', hour);
+    $planBlocks.append($planRow);
+    }
+    // getting todos from localStorage. JSON parse string into object
+    let savedPlans = JSON.parse(localStorage.getItem("savedPlans"));
+    if (test) {
+        console.log(savedPlans);
+    }
+    // local storage plans update plan array
+    if (savedPlans !== null) {
+        planArray = savedPlans;
+    } else {
+        planArray = new Array(9);
+    }
+
 
 });
