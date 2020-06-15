de05ee876b446224faddad015b7149e206b298e0
Phuong Le <phuonganh.le0312@gmail.com>
2020-06-15 17:56:06 -0400

    changed order of jquery file and added 24 and 12 hour variables from moment

--- script.js
+++ script.js
@@ -2,10 +2,18 @@ $(document).ready(function() {
 // current time from moment
 const currentTime = moment().format('MMMM Do YYYY, h:mm:ss a');
 //made date heading the current time
-let $dateHeading = $("#currentDay");
+let $dateHeading = $('#currentDay');
 $dateHeading.text(currentTime);
 // testing variable
 const test= false;
+
+let nowHour24 = moment().format('H');
+let nowHour12 = moment().format('h');
+
+if (test){
+    nowHour24= 13;
+    nowHour12= 1;
+}
 
 
 
