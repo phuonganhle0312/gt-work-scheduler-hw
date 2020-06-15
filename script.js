d01a401337c5aa1434154314ea0e142b837474cb
Phuong Le <phuonganh.le0312@gmail.com>
2020-06-15 18:11:21 -0400

    changed order of jquery file and added 24 and 12 hour variables from moment. set var to get todos from local storage and update plans to a plan array.

--- script.js
+++ script.js
@@ -6,15 +6,22 @@ let $dateHeading = $('#currentDay');
 $dateHeading.text(currentTime);
 // testing variable
 const test= false;
-
+//test in non standard hours
 let nowHour24 = moment().format('H');
 let nowHour12 = moment().format('h');
-
+//setting time for non standard hours
 if (test){
     nowHour24= 13;
     nowHour12= 1;
 }
-
-
+//getting todos from localStorage. JSON parse string into object
+let savedPlans = JSON.parse(localStorage.getItem("savedPlans"));
+if (test) {console.log(savedPlans);}
+//local storage plans update plan array
+if (savedPlans !== null){
+    planArray = savedPlans;
+}else {
+    planArray=new Array(9);
+}
 
 });
